51cdd0aec3f95f0c409579e2fc2e24111f949c7c
lenidar <55443074+lenidar@users.noreply.github.com>
2023-04-17 15:26:46 +0800

    Add project files.

---
+++ StreamWriterDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamWriterDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool append = true;
+            string filename = "sample5";
+            string fileExt = "txt";
+            while (true)
+            {
+                using (StreamWriter sw 
+                    = new StreamWriter(filename + "." + fileExt, append))
+                {
+                    string input = Console.ReadLine();
+                    sw.WriteLine(input);
+                }
+                //Console.ReadKey();
+            }
+        }
+    }
+}
